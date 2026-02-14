2ff14df1ea465e954fde64594a42b98c7d227222
PR creations <91177786+Pushkar456@users.noreply.github.com>
2026-02-14 16:03:24 +0530

    Update SectionHeading.tsx
    remove unwanted lines of code

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,33 +1,3 @@
-// interface SectionHeadingProps {
-//   title: string;
-// }
-
-// const SectionHeading = ({ title }: SectionHeadingProps) => {
-//   return (
-//     <h2
-//       className="
-//         text-3xl
-//         sm:text-4xl
-//         md:text-5xl
-//         lg:text-6xl
-//         font-extrabold
-//         bg-gradient-to-r
-//         from-neutral-400
-//         via-neutral-200
-//         to-neutral-400
-//         bg-clip-text
-//         text-transparent
-//         tracking-tight
-//         text-center
-//       "
-//     >
-//       {title}
-//     </h2>
-//   );
-// };
-
-// export default SectionHeading;
-
 
 "use client";
 import { motion } from "framer-motion";
